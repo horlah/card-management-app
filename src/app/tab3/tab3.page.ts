1f0ee9f05abfa84e681ff8bf609f3726797608d5
emmanuel <emmanuel.ainaj@gmail.com>
2019-04-07 19:40:53 +0100

    build user profile

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+    selector: 'app-tab3',
+    templateUrl: 'tab3.page.html',
+    styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+    constructor(
+        private toastController: ToastController,
+        private loadingController: LoadingController
+        ) { }
+
+    async updateProfile() {
+        const loading = await this.loadingController.create({
+            message: 'updating...',
+            duration: 2000
+        });
+        await loading.present();
+
+        const toast = await this.toastController.create({
+            message: 'Your profile has been updated successfully.',
+            duration: 4000
+        });
+
+        loading.onDidDismiss().then(() => toast.present());
+    }
+}
